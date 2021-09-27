9569565337503940e2d55964f3c0e4db2946f3d7
shaka1082 <66804236+shaka1082@users.noreply.github.com>
2021-09-27 17:40:32 -0300

    INICIO DO MEU CURSO

---
+++ atividade 2.py
@@ -0,0 +1,7 @@
+data = input ('Em que dia você nasceu?:')
+mes  = input ('Em que mês você nasceu?:')
+ano = input ('Em que ano você nasceu?:')
+
+print( ' Você nasceu no dia ' + data + ' do mês ' + mes + ' de ' + ano)
+
+
